8fe81d75f79ac63e435c611b84f24f8eb83276d1
Adam Kabbeke <akabbeke@gmail.com>
2014-03-07 15:29:42 -0500

    made the slicing work for any degree tessellation

--- golpy.py
+++ golpy.py
@@ -3,9 +3,10 @@ import numpy
 import Image
 import pp
 
-def lifeStep(X):
+def lifeStep(X,m):
 
     # Game of life step using generator expressions
+
     X = numpy.asarray(X)
     assert X.ndim == 2
     X = X.astype(bool)
@@ -14,11 +15,12 @@ def lifeStep(X):
                      for i in (-1, 0, 1) for j in (-1, 0, 1)
                      if (i != 0 or j != 0))
     X = (nbrs_count == 3) | (X & (nbrs_count == 2))
-    return tessellate(X,2)
+
+    return tessellate(X,m)
 
 def tessellate(X,n):
 
-	# Tesselates the field into n*n subdevisions
+	# Tesselates the field X into n*n subdevisions
 
 	Xn = numpy.zeros(numpy.array(X.shape)*n)
 
@@ -27,53 +29,60 @@ def tessellate(X,n):
 			Xn[i*n:(i+1)*n,j*n:(j+1)*n] = X[i,j]*numpy.ones((n,n))
 	return Xn
 
-def sliceX(X):
+def sliceX(X,m):
 
-	# Slices the X into smaller chunks
+	# Slices the X into smaller chunks to be worked on by each core
 
 	return(numpy.delete(numpy.delete(X,range(X.shape[1]/2+1,X.shape[1]-1),axis=1),range(X.shape[0]/2+1,X.shape[0]-1),axis=0),
 			numpy.delete(numpy.delete(X,range(1,X.shape[1]/2-1),axis=1),range(X.shape[0]/2+1,X.shape[0]-1),axis=0),
 			numpy.delete(numpy.delete(X,range(X.shape[1]/2+1,X.shape[1]-1),axis=1),range(1,X.shape[0]/2-1),axis=0),
 			numpy.delete(numpy.delete(X,range(1,X.shape[1]/2-1),axis=1),range(1,X.shape[0]/2-1),axis=0))
 
-def iterate(X,name,n):
+def iterate(X,name,n,m):
 
 	# Iterates the field n times
+	# X = field
+	# name = file name Prefix
+	# n = number of life cycles
+	# m = tessellation factor
 
 	ppservers = ()
 
 	# Creates jobserver with automatically detected number of workers
-	job_server = pp.Server(ppservers=ppservers)
+
+	jobServer = pp.Server(ppservers=ppservers)
 
 
 	for q in range(n):
 
-		arrayList = sliceX(X)
+		arrayList = sliceX(X,m)
+		jobs = [jobServer.submit(lifeStep, (part,m), (tessellate,), ("numpy",))() for part in arrayList]
 
-		jobs = [(job_server.submit(lifeStep, (part,), (tessellate,), ("numpy",))) for part in arrayList]
+		X = numpy.vstack([numpy.hstack([jobs[0][:jobs[0].shape[0]-(2*m),:jobs[0].shape[1]-(2*m)],
+		 								jobs[1][:jobs[1].shape[0]-(2*m),(2*m):]]),
+		 				  numpy.hstack([jobs[2][(2*m):,:jobs[3].shape[1]-(2*m)],
+		 								jobs[3][(2*m):,(2*m):]])])
+	if n-(q+1) == 0: 
+		Xi = X
+	else:
+		Xi = tessellate(X,m**(n-(q+1)))
 
-		wat = [job() for job in jobs]
+	jobServer.print_stats()
+	
+	img = Image.new( 'RGB', Xi.shape, "black")
+	pixels = img.load()
 
-		X = numpy.vstack([numpy.hstack([wat[0][:wat[0].shape[0]-4,:wat[0].shape[1]-4],
-										wat[1][:wat[1].shape[0]-4,4:]]),
-						  numpy.hstack([wat[2][4:,:wat[3].shape[1]-4],
-										wat[3][4:,4:]])])
-		print X
-		job_server.print_stats()
-		
-		img = Image.new( 'RGB', X.shape, "white")
-		pixels = img.load()
+	for i in range(img.size[0]):
+		for j in range(img.size[1]):
+			if Xi[i,j] == 1:
+				pixels[i,j] = ((i*255)/img.size[0],(j*255)/img.size[1],100) # set the colour
 
-		for i in range(img.size[0]):
-			for j in range(img.size[1]):
-				if X[i,j] == 1:
-					pixels[i,j] = (0,0,0) # set the colour
-
-		img.save(name+str(q)+'.png')
+	img.save(name+str(q)+'.png')
 
 def main():
-	X= numpy.array([[0,0,0,0],[0,1,1,0],[0,1,1,0],[0,0,0,0]])
-	iterate(X,'Renders/2x2-',11)
+
+	X = numpy.array([[1,0,0,0,0,1],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[1,0,0,0,0,1]])
+	iterate(X,'Renders/Ctest2',10,2)
 
 if __name__ == "__main__":
     main()
